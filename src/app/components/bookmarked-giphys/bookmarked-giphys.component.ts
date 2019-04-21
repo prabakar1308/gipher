3e4989c7adb9be2d520e2c3cbb52e3cd9355a0ba
Mohanakrishnan Ashokan <M1028218@mindtree.com>
2019-04-21 19:07:25 +0530

    Cosmetic changes

--- src/app/components/bookmarked-giphys/bookmarked-giphys.component.ts
+++ src/app/components/bookmarked-giphys/bookmarked-giphys.component.ts
@@ -1,44 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-bookmarked-giphys',
-  templateUrl: './bookmarked-giphys.component.html',
-  styleUrls: ['./bookmarked-giphys.component.css']
+  selector: "app-bookmarked-giphys",
+  templateUrl: "./bookmarked-giphys.component.html",
+  styleUrls: ["./bookmarked-giphys.component.css"]
 })
 export class BookmarkedGiphysComponent implements OnInit {
-  favouriteGiphys = [
-    {
-      title: 'Praba',
-      subTitle: 'Cat',
-      imgSrc: 'https://media2.giphy.com/media/FiGiRei2ICzzG/200.gif'
-    },
-    {
-      title: 'Mohan',
-      subTitle: 'Cat',
-      imgSrc: 'https://media0.giphy.com/media/feqkVgjJpYtjy/200.gif'
-    },
-    {
-      title: 'Test',
-      subTitle: 'Cat',
-      imgSrc: 'https://media2.giphy.com/media/FiGiRei2ICzzG/200.gif'
-    },
-    {
-      title: 'Test',
-      subTitle: 'Cat',
-      imgSrc: 'https://media2.giphy.com/media/FiGiRei2ICzzG/200.gif'
-    },
-    {
-      title: 'Test',
-      subTitle: 'Cat',
-      imgSrc: 'https://media2.giphy.com/media/FiGiRei2ICzzG/200.gif'
-    },
-    {
-      title: 'Mohan',
-      subTitle: 'Cat',
-      imgSrc: 'https://media0.giphy.com/media/feqkVgjJpYtjy/200.gif'
-    }
-  ];
+  favouriteGiphys: [];
+  stageArray = [];
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.favouriteGiphys = JSON.parse(localStorage.getItem("favoriteGiphies"));
+  }
 }
--- src/app/components/giphy-list/giphy-list.component.ts
+++ src/app/components/giphy-list/giphy-list.component.ts
@@ -1,14 +1,23 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-giphy-list',
-  templateUrl: './giphy-list.component.html',
-  styleUrls: ['./giphy-list.component.css']
+  selector: "app-giphy-list",
+  templateUrl: "./giphy-list.component.html",
+  styleUrls: ["./giphy-list.component.css"]
 })
 export class GiphyListComponent implements OnInit {
   @Input() giphyList: any;
+  giphySource: {};
+  stageArray = [];
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
+
+  bookMarkSelected(giphy) {
+    localStorage.setItem("selectedGiphy", JSON.stringify(giphy));
+    this.stageArray.push(JSON.parse(localStorage.getItem("selectedGiphy")));
+    localStorage.setItem("favoriteGiphies", JSON.stringify(this.stageArray));
+  }
 }
