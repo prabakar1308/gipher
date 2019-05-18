67cbcf05f385d8655a574997495b7ed61d0cbbe7
Mohanakrishnan Ashokan <M1028218@mindtree.com>
2019-05-18 20:55:36 +0530

    updated onBookmark method

--- src/app/components/giphy-list/giphy-list.component.ts
+++ src/app/components/giphy-list/giphy-list.component.ts
@@ -23,44 +23,30 @@ export class GiphyListComponent implements OnInit {
   }
 
   onBookmark(giphy) {
-    const {
-      id,
-      title,
-      images: {
-        original: { url }
-      }
-    } = giphy;
-    JSON.parse(localStorage.getItem('currentUser'));
-    const { username } = JSON.parse(localStorage.getItem('currentUser'));
-    const giphyReq = {
-      userId: username,
-      title,
-      imageId: id,
-      imageUrl: url
-    };
-
-    //  const favourites = JSON.parse(localStorage.getItem('favoriteGiphies'));
-    //  let newFavourites = [];
     if (this.bookmark) {
-      // const index = favourites.findIndex(gip => gip.id === giphyObj.id);
-      // if (index > -1) {
-      //   favourites.splice(index, 1);
-      //   newFavourites = [...favourites];
-      //   this.giphyList = newFavourites;
-      // }
       this.svc.deleteBookmarked(giphy.id).subscribe(response => {
         console.log('Removed Sucessfully');
       });
     } else {
-      // newFavourites = [
-      //   ...JSON.parse(localStorage.getItem('favoriteGiphies')),
-      //   giphy
-      // ];
+      const {
+        id,
+        title,
+        images: {
+          original: { url }
+        }
+      } = giphy;
+      JSON.parse(localStorage.getItem('currentUser'));
+      const { username } = JSON.parse(localStorage.getItem('currentUser'));
+      const giphyReq = {
+        userId: username,
+        title,
+        imageId: id,
+        imageUrl: url
+      };
       this.svc.saveBookmarked([giphyReq]).subscribe(response => {
         console.log('bookmarked sucessfully');
       });
     }
-    // localStorage.setItem('favoriteGiphies', JSON.stringify(newFavourites));
   }
 
   isGiphyExists(giphy) {
